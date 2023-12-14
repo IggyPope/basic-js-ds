060dd0e5b203d48ac8dd53358b33cf976555e787
IggyPope <altko4egar@gmail.com>
2023-12-14 17:36:37 +0300

    feat: implement BinarySearchTree.root() and BinarySearchTree.add(data) methods

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,40 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
-
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.rootNode = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  root() {
+    return this.rootNode;
+  }
+
+  add(data) {
+    this.rootNode = insert(this.rootNode, data);
+
+    function insert(node, data) {
+      if (!node) {
+        return new Node(data);
+      }
+
+      if (data === node.data) {
+        return node;
+      }
+
+      if (data < node.data) {
+        node.left = insert(node.left, data);
+      } else {
+        node.right = insert(node.right, data);
+      }
+
+      return node;
+    }
   }
 
   has(/* data */) {
@@ -45,5 +64,5 @@ class BinarySearchTree {
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
